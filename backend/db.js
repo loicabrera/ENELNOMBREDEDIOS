7412709f3d5b31dac74d2a95510f2950df3743f7
raimy2 <raimyguzman0066ceges@gmail.com>
2025-05-15 14:43:40 -0400

    funcionooooo

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,15 @@
-// backend/db.js
 import { Sequelize } from 'sequelize';
 
-// Crea la instancia de Sequelize (esto maneja la conexión)
-const conexion = new Sequelize('prueba', 'root', '123456', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+const conexion = new Sequelize(
+  'bad7nkcdovetz0xcd7tt',    // reemplaza con MYSQL_ADDON_DB
+  'uj3nqin9zmxkkhr4',                 // reemplaza con MYSQL_ADDON_USER
+  '51GsplIbrfLHDFhNd29',              // reemplaza con MYSQL_ADDON_PASSWORD
+  {
+    host: 'bad7nkcdovetz0xcd7tt-mysql.services.clever-cloud.com',            // reemplaza con MYSQL_ADDON_HOST
+    port: 20695,              // verifica si es diferente
+    dialect: 'mysql',
+    logging: false,          // o true si quieres ver logs
+  }
+);
 
 export default conexion;
